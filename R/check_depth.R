3d98adcd5343e5c7f58f2c3af187d79723772359
Samuel Bosch <mail@samuelbosch.com>
2017-11-20 12:07:13 +0100

    Check depth: first tests

--- R/check_depth.R
+++ R/check_depth.R
@@ -1,31 +1,36 @@
-add_depth_error <- function(report, column, i, message) {
+add_depth_error <- function(result, column, i, message) {
   if (length(i) > 0) {
-    report <- rbind(report, list(field = column, level = 'error', row = i, message = message))
+    result <- rbind(result, data.frame(field = column, level = 'error', row = i, message = message, stringsAsFactors = FALSE))
   }
-  return(report)
+  return(result)
 }
 
-check_depth_column <- function(report, data, column, lookupvalues, depthmargin, shoremargin) {
+check_depth_column <- function(result, data, column, lookupvalues, depthmargin, shoremargin) {
   if (column %in% colnames(data)) {
-    depths <- as.numeric(data[,column])
+    depths <- as.numeric(as.character(data[,column]))
     if(all(data[,column] == '')) {
-      report <- rbind(report, list(level = 'warning',
+      result <- rbind(result, list(level = 'warning',
                                    message = paste('Column',column,'empty')))
     }
     invalid <- which(is.na(depths) & data[,column] != '')
-    report <- add_depth_error(report, column, invalid, 'Depth value is not numeric and not empty')
+    result <- add_depth_error(result, column, invalid, 'Depth value is not numeric and not empty')
 
-    gridwrong <- which(!is.na(depths) & depths > (lookupvalues$bathymetry + depthmargin))
-    report <- add_depth_error(report, column, gridwrong, paste0('Depth value is larger than the value found in the bathymetry raster (depth margin=',depthmargin,')'))
+    gridwrong <- which(!is.na(depths) & depths > (lookupvalues$bathymetry + rep(depthmargin, nrow(lookupvalues))))
+    result <- add_depth_error(result, column, gridwrong, paste0('Depth value is greater than the value found in the bathymetry raster (depth margin=',depthmargin,')'))
 
-    negativewrong <- which(!is.na(depths) & depths < 0 & (lookupvalues$shoredistance - shoremargin > 0))
-    report <- add_depth_error(report, column, negativewrong, paste0('Depth value is negative than the value found in the bathymetry raster (shoredistance margin=', shoremargin,')'))
+    if(!is.na(shoremargin)) {
+      negativewrong <- which(!is.na(depths) & depths < 0 & ((lookupvalues$shoredistance - rep(shoremargin, nrow(lookupvalues))) > 0))
+      result <- add_depth_error(result, column, negativewrong, paste0('Depth value is negative for offshore points (shoredistance margin=', shoremargin,')'))
+
+      positivewrong <- which(!is.na(depths) & depths > 0 & ((lookupvalues$shoredistance + rep(shoremargin, nrow(lookupvalues))) < 0))
+      result <- add_depth_error(result, column, positivewrong, paste0('Depth value is positive for on land points (shoredistance margin=', shoremargin,')'))
+    }
   } else {
-    report <- rbind(report, list(level = 'warning',
+    result <- rbind(result, list(level = 'warning',
                                  field = column,
                                  message = paste('Column',column,'missing')))
   }
-  return(report)
+  return(result)
 }
 
 
@@ -37,14 +42,14 @@ check_depth_column <- function(report, data, column, lookupvalues, depthmargin, 
 #' @param depthmargin How much can the given depth deviate from the bathymetry
 #'   in the rasters (in meters).
 #' @param shoremargin How far offshore should a record be to have a bathymetry
-#'   larger then 0.
+#'   larger then 0. If \code{NA} (default) then this test is ignored
 #' @param report If TRUE, errors are returned instead of records.
 #'
 #' @return Errors or records.
 #' @export
-check_depth <- function(data, depthmargin = 0, shoremargin = 0, report = FALSE) {
-  lookupvalues <- lookup_xy(data, shoredistance = TRUE, areas = FALSE)
-  report <- data.frame(
+check_depth <- function(data, depthmargin = 0, shoremargin = NA, report = FALSE) {
+  lookupvalues <- lookup_xy(data, shoredistance = !is.na(shoremargin), grids = TRUE, areas = FALSE)
+  result <- data.frame(
     level = character(),
     row = integer(),
     field = character(),
@@ -53,20 +58,20 @@ check_depth <- function(data, depthmargin = 0, shoremargin = 0, report = FALSE) 
   )
 
   depthcols <- c('minimumDepthInMeters', 'maximumDepthInMeters')
-  for(col in depthcols) {
-    report <- check_depth_column(report, data, col, lookupvalues, depthmargin, shoremargin)
+  for(column in depthcols) {
+    result <- check_depth_column(result, data, column, lookupvalues, depthmargin, shoremargin)
   }
 
-  if(all(depthcols) %in% colnames(data)) {
-    mind <- as.numeric(data[depthcols[1],])
-    maxd <- as.numeric(data[depthcols[2],])
+  if(all(depthcols %in% colnames(data))) {
+    mind <- as.numeric(as.character(data[,depthcols[1]]))
+    maxd <- as.numeric(as.character(data[,depthcols[2]]))
     minGTmax <- which(!is.na(maxd) & !is.na(mind) & mind > maxd)
-    report <- add_depth_error(report, depthcols, minGTmax, "Minimum depth is larger then maximum depth")
+    result <- add_depth_error(result, depthcols, minGTmax, "Minimum depth is greater than maximum depth")
   }
 
   if (report) {
-    return(report)
+    return(result)
   } else {
-    return(data[na.omit(report$row),])
+    return(data[sort(unique(na.omit(result$row))),])
   }
 }
--- tests/testthat/test_check_depth.R
+++ tests/testthat/test_check_depth.R
@@ -1,21 +1,47 @@
 library(obistools)
 context("check depth")
 
-test_data <- data.frame(
-  decimalLongitude=c(0),
-  decimalLatitude=c(0),
-  minimumDepthInMeters = c("10", "", "5"),
-  maximumDepthInMeters = c("10", "", "5")
-)
 
 test_that("check_depth detects invalid or impossible depth values", {
-
-  # TEST CASES TO TEST
+  # Test cases
+  # empty dataframe
   # Min D > Max D
-  # Min D / Max D missing
-  # Min D / Max D empty
-  # Min D / Max D not numeric and not ''
-  # Min D / Max D > bathy (and check depthmargin)
-  # Min D / Max D < 0 & shoredist > 0 (and check shoremargin)
+  # Min D | Max D missing
+  # Min D | Max D empty
+  # Min D | Max D not numeric and not ''
+  # Min D | Max D > bathy (and check depthmargin)
+  # Min D | Max D < 0 & shoredist > 0 (and check shoremargin)
+  # report: TRUE/FALSE
+  # -> + check the above for on land points
+  t1 <- data.frame(
+    decimalLongitude=c(0),
+    decimalLatitude=c(0),
+    minimumDepthInMeters = c("4936", "4938", "4935", "-20"),
+    maximumDepthInMeters = c("4936", "4937", "5000", "-10")
+  )
+  d1 <- check_depth(t1, depthmargin = 0, shoremargin = NA, report = FALSE)
+  expect_equal(2, nrow(d1))
+  expect_equal(c("4938", "5000"), as.character(d1$minimumDepthInMeters))
+  r1 <- check_depth(t1, depthmargin = 0, shoremargin = NA, report = TRUE)
+  expect_equal(5, nrow(r1))
+  expect_equal(3, sum(grepl("depth margin", r1$message)))
+  expect_equal(2, sum(grepl("greater than maximum", r1$message)))
 
+  r2 <- check_depth(t1, depthmargin = 0, shoremargin = 1e6, report = TRUE)
+  expect_equal(r1, r2)
+  r3 <- check_depth(t1, depthmargin = 0, shoremargin = 0, report = TRUE)
+  expect_equal(7, nrow(r3))
+  expect_equal(3, sum(grepl("depth margin", r3$message)))
+  expect_equal(2, sum(grepl("greater than maximum", r3$message)))
+  expect_equal(2, sum(grepl("negative", r3$message)))
+
+
+  t2 <- data.frame(decimalLongitude=c(0), decimalLatitude=c(0), minimumDepthInMeters=c("10"))
+  check_depth(t2, depthmargin = 0, shoremargin = 0, report = FALSE)
+  t3 <- data.frame(decimalLongitude=c(0), decimalLatitude=c(0), minimumDepthInMeters=c("10"))
+  check_depth(t3, depthmargin = 0, shoremargin = 0, report = FALSE)
+  t4 <- data.frame(decimalLongitude=c(0), decimalLatitude=c(0), minimumDepthInMeters=c(""), maximumDepthInMeters=c(""))
+  check_depth(t4, depthmargin = 0, shoremargin = 0, report = FALSE)
+  t5 <- data.frame(decimalLongitude=c(0), decimalLatitude=c(0), minimumDepthInMeters=c("10"), maximumDepthInMeters=c(""))
+  check_depth(t5, depthmargin = 0, shoremargin = 0, report = FALSE)
 })
